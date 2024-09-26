580e21f6515f8bf9b9b6ade91181e63d30a25145
Alejandromp <diegomartinez1101@gmail.com>
2024-09-26 13:54:45 -0500

    Add crud products

--- routes/products.js
+++ routes/products.js
@@ -33,4 +33,58 @@ router.post('/', async (req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+    const product =  await model.findOne({nombre: id})
+    if (!product) {
+      return res.status(404).json({ message: 'producto no encontrado' });
+    }
+    res.json(product);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Error al leer el producto' });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+
+  const id = req.params.id;
+
+  try {
+    const product = await model.findOne({ nombre: id });
+    if (!product) {
+      return res.status(404).json({ message: 'Producto no encontrado' });
+    }
+
+    const updates = {
+      nombre: req.body.nombre,
+      cantidad: req.body.cantidad,
+      valor: req.body.valor,
+      categoria: req.body.categoria,
+      descripcion: req.body.descripcion,
+      imagen: req.body.imagen,
+    };
+
+    await model.updateOne({ nombre: id }, { $set: updates });
+    res.json({ message: 'Producto actualizado con éxito' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Error al actualizar el Producto' });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+
+  const id = req.params.id;
+
+  try {
+    await model.deleteOne({ nombre: id });
+    res.json({ message: 'Producto eliminado con éxito' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Error al eliminar Producto' });
+  }
+});
+
 module.exports = router;
