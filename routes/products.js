95b69b4e7204ce61794cc8553a37307832c600c8
Alejandromp <diegomartinez1101@gmail.com>
2024-09-26 18:32:03 -0500

    Config to deploy in vercel

--- routes/products.js
+++ routes/products.js
@@ -29,7 +29,7 @@ router.post('/', async (req, res) => {
     res.json({ message: 'Producto creado con éxito' });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Error al crear un productos' });
+    res.status(500).json({ message: 'Error al crear un producto' });
   }
 });
 
