3f2cd557ba11d6f7ec813f35942bd066f18a8599
Alejandromp <diegomartinez1101@gmail.com>
2024-10-09 12:10:09 -0500

    Change method login

--- routes/users.js
+++ routes/users.js
@@ -13,7 +13,7 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/login', async (req, res) => {
+router.post('/login', async (req, res) => {
   const {email,contrasena} = req.body;
 
   if (!email || !contrasena) {
