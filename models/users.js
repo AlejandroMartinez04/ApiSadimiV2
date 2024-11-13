df7228c38e0b7ddc584860519aea6b2f39f36e60
DiegoMartinez <diegomartinez1101@mail.com>
2024-11-13 11:20:26 -0500

    Add no validate

--- models/users.js
+++ models/users.js
@@ -1,48 +1,48 @@
 const mongoose = require('mongoose');
 
 const direccionsSchema = new mongoose.Schema({
-  departamento: { type: String, required: true },
-  ciudad: { type: String, required: true },
-  direccion: { type: String, required: true }
+  departamento: { type: String },
+  ciudad: { type: String },
+  direccion: { type: String }
 });
 
 const paymentSchema = new mongoose.Schema({
-  numberCard: { type: Number, required: true},
-  dateExp: { type: String, required: true},
-  cvv: { type: Number, required: true},
+  numberCard: { type: Number, required: true },
+  dateExp: { type: String, required: true },
+  cvv: { type: Number, required: true },
   favorite: { type: Boolean, default: false }
 });
 
 const sellSchema = new mongoose.Schema({
   date: { type: Date, required: true },
   product: [],
-  payment: { type: String, required: true},
-  total: { type: Number, required: true }  
+  payment: { type: String, required: true },
+  total: { type: Number, required: true }
 });
 
-  const userSchema = new mongoose.Schema({
-    nombre: { type: String, required: true },
-    apellido: { type: String, required: true },
-    documento: {
-      type: Number,
-      required: true,
-      unique: true,
-      validate: {
-        validator: function(v) {
-          return v !== null && v !== undefined;
-        },
-        message: 'El documento no puede ser nulo o vacío'
-      }
-    },
-    telefono: { type: Number, required: true },
-    email: { type: String, required: true , unique: true},
-    contrasena: { type: String, required: true },
-    direcciones: [direccionsSchema],
-    direccionFavorita: { type: mongoose.Schema.Types.ObjectId, ref: 'Address' },
-    metodoPago: [paymentSchema],
-    pagoFavorito: { type: mongoose.Schema.Types.ObjectId, ref: 'Payment' },
-    ventas: [sellSchema]
-  });
+const userSchema = new mongoose.Schema({
+  nombre: { type: String, required: true },
+  apellido: { type: String, required: true },
+  documento: {
+    type: Number,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function (v) {
+        return v !== null && v !== undefined;
+      },
+      message: 'El documento no puede ser nulo o vacío'
+    }
+  },
+  telefono: { type: Number, required: true },
+  email: { type: String, required: true, unique: true },
+  contrasena: { type: String, required: true },
+  direcciones: [direccionsSchema],
+  direccionFavorita: { type: mongoose.Schema.Types.ObjectId, ref: 'Address' },
+  metodoPago: [paymentSchema],
+  pagoFavorito: { type: mongoose.Schema.Types.ObjectId, ref: 'Payment' },
+  ventas: [sellSchema]
+});
 
 module.exports = mongoose.model('users', userSchema);
 
